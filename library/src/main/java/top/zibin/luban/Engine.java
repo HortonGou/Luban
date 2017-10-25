35191dbe3fba3e71c05ce1de56b218d7702af9ec
ght <597315261@qq.com>
2017-10-25 11:37:43 +0800

    旋转改为使用support中的exifinterface读取 使用自定的图片大小调整方法，防止重复压缩，增大尺寸调整的比重 调整质量压缩率，尽量保持原图质量 大图使用70%质量，小图使用90%质量

--- library/src/main/java/top/zibin/luban/Engine.java
+++ library/src/main/java/top/zibin/luban/Engine.java
@@ -3,7 +3,9 @@ package top.zibin.luban;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-import android.media.ExifInterface;
+import android.support.annotation.NonNull;
+import android.support.media.ExifInterface;
+import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -14,93 +16,174 @@ import java.io.IOException;
  * Responsible for starting compress and managing active and cached resources.
  */
 class Engine {
-  private ExifInterface srcExif;
-  private String srcImg;
-  private File tagImg;
-  private int srcWidth;
-  private int srcHeight;
+    private ExifInterface srcExif;
+    private String srcImg;
+    private File tagImg;
+    private int srcWidth;
+    private int srcHeight;
 
-  Engine(String srcImg, File tagImg) throws IOException {
-    if (Checker.isJPG(srcImg)) {
-      this.srcExif = new ExifInterface(srcImg);
-    }
-    this.tagImg = tagImg;
-    this.srcImg = srcImg;
+    Engine(String srcImg, File tagImg) throws IOException {
+        if (Checker.isJPG(srcImg)) {
+            this.srcExif = new ExifInterface(srcImg);
+        }
+        this.tagImg = tagImg;
+        this.srcImg = srcImg;
 
-    BitmapFactory.Options options = new BitmapFactory.Options();
-    options.inJustDecodeBounds = true;
-    options.inSampleSize = 1;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inSampleSize = 1;
 
-    BitmapFactory.decodeFile(srcImg, options);
-    this.srcWidth = options.outWidth;
-    this.srcHeight = options.outHeight;
-  }
-
-  private int computeSize() {
-    srcWidth = srcWidth % 2 == 1 ? srcWidth + 1 : srcWidth;
-    srcHeight = srcHeight % 2 == 1 ? srcHeight + 1 : srcHeight;
-
-    int longSide = Math.max(srcWidth, srcHeight);
-    int shortSide = Math.min(srcWidth, srcHeight);
-
-    float scale = ((float) shortSide / longSide);
-    if (scale <= 1 && scale > 0.5625) {
-      if (longSide < 1664) {
-        return 1;
-      } else if (longSide >= 1664 && longSide < 4990) {
-        return 2;
-      } else if (longSide > 4990 && longSide < 10240) {
-        return 4;
-      } else {
-        return longSide / 1280 == 0 ? 1 : longSide / 1280;
-      }
-    } else if (scale <= 0.5625 && scale > 0.5) {
-      return longSide / 1280 == 0 ? 1 : longSide / 1280;
-    } else {
-      return (int) Math.ceil(longSide / (1280.0 / scale));
-    }
-  }
-
-  private Bitmap rotatingImage(Bitmap bitmap) {
-    if (srcExif == null) return bitmap;
-
-    Matrix matrix = new Matrix();
-    int angle = 0;
-    int orientation = srcExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-    switch (orientation) {
-      case ExifInterface.ORIENTATION_ROTATE_90:
-        angle = 90;
-        break;
-      case ExifInterface.ORIENTATION_ROTATE_180:
-        angle = 180;
-        break;
-      case ExifInterface.ORIENTATION_ROTATE_270:
-        angle = 270;
-        break;
+        BitmapFactory.decodeFile(srcImg, options);
+        this.srcWidth = options.outWidth;
+        this.srcHeight = options.outHeight;
     }
 
-    matrix.postRotate(angle);
+    private int computeSize() {
+        srcWidth = srcWidth % 2 == 1 ? srcWidth + 1 : srcWidth;
+        srcHeight = srcHeight % 2 == 1 ? srcHeight + 1 : srcHeight;
 
-    return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-  }
+        int longSide = Math.max(srcWidth, srcHeight);
+        int shortSide = Math.min(srcWidth, srcHeight);
 
-  File compress() throws IOException {
-    BitmapFactory.Options options = new BitmapFactory.Options();
-    options.inSampleSize = computeSize();
+        float scale = ((float) shortSide / longSide);
+        if (scale <= 1 && scale > 0.5625) {
+            if (longSide < 1664) {
+                return 1;
+            } else if (longSide >= 1664 && longSide < 4990) {
+                return 2;
+            } else if (longSide > 4990 && longSide < 10240) {
+                return 4;
+            } else {
+                return longSide / 1280 == 0 ? 1 : longSide / 1280;
+            }
+        } else if (scale <= 0.5625 && scale > 0.5) {
+            return longSide / 1280 == 0 ? 1 : longSide / 1280;
+        } else {
+            return (int) Math.ceil(longSide / (1280.0 / scale));
+        }
+    }
 
-    Bitmap tagBitmap = BitmapFactory.decodeFile(srcImg, options);
-    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    private float calculateScaleSize(Bitmap bitmap) {
+//        srcWidth = srcWidth % 2 == 1 ? srcWidth + 1 : srcWidth;
+//        srcHeight = srcHeight % 2 == 1 ? srcHeight + 1 : srcHeight;
 
-    tagBitmap = rotatingImage(tagBitmap);
-    tagBitmap.compress(Bitmap.CompressFormat.JPEG, 60, stream);
-    tagBitmap.recycle();
+        int longSide = Math.max(bitmap.getWidth(), bitmap.getHeight());
+        int shortSide = Math.min(bitmap.getWidth(), bitmap.getHeight());
 
-    FileOutputStream fos = new FileOutputStream(tagImg);
-    fos.write(stream.toByteArray());
-    fos.flush();
-    fos.close();
-    stream.close();
+        float scale = ((float) shortSide / longSide);
+        if (scale >= 0.5f && longSide > 1280) {
+            return 1280f / longSide;
+        } else if (scale < 0.5f && shortSide > 1280) {
+            return 1280f / shortSide;
+        }
+        return 1f;
+    }
 
-    return tagImg;
-  }
+    private Bitmap getUnOutOfMemoryBitmap() {
+        int inSampleSize = 1;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inSampleSize = 1;
+
+        BitmapFactory.decodeFile(srcImg, options);
+        int srcWidth = options.outWidth;
+        int srcHeight = options.outHeight;
+        int w = srcWidth;
+        int h = srcHeight;
+        while (!canCreate(w, h)) {
+            inSampleSize++;
+            w = srcWidth / inSampleSize;
+            h = srcHeight / inSampleSize;
+        }
+        BitmapFactory.Options options2 = new BitmapFactory.Options();
+        options2.inSampleSize = inSampleSize;
+        return BitmapFactory.decodeFile(srcImg, options2);
+    }
+
+    File compress() throws IOException {
+        //采样
+        Bitmap tagBitmap = getUnOutOfMemoryBitmap();
+        //缩放
+        float scale = calculateScaleSize(tagBitmap);
+        Log.e("luban", "scale : " + scale);
+        if (scale != 1f) {
+            Matrix matrix = new Matrix();
+            matrix.setScale(scale, scale);
+            tagBitmap = transformBitmap(tagBitmap, matrix);
+        }
+        //旋转
+        tagBitmap = rotatingImage(tagBitmap);
+        //压缩
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        if (tagBitmap.getWidth() > 1280 || tagBitmap.getHeight() > 1280) {
+            tagBitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+        } else {
+            tagBitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+        }
+
+        tagBitmap.recycle();
+
+        FileOutputStream fos = new FileOutputStream(tagImg);
+        fos.write(stream.toByteArray());
+        fos.flush();
+        fos.close();
+        stream.close();
+
+        return tagImg;
+    }
+
+    private Bitmap transformBitmap(@NonNull Bitmap bitmap, @NonNull Matrix transformMatrix) {
+        try {
+            Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), transformMatrix, false);
+            if (!bitmap.sameAs(converted)) {
+                bitmap = converted;
+            }
+        } catch (OutOfMemoryError error) {
+            Log.e("luban", "transformBitmap: " + error);
+        }
+        return bitmap;
+    }
+
+    private boolean canCreate(int w, int h) {
+        boolean canScale = checkMemory(w, h);
+        if (!canScale) {
+            Log.e("Luban", "no enough memory!");
+        }
+        return canScale;
+    }
+
+    private boolean checkMemory(int width, int height) {
+        Runtime runtime = Runtime.getRuntime();
+        long free = runtime.maxMemory() - runtime.totalMemory() + runtime.freeMemory();
+        int allocation = (width * height) << 2;
+        Log.d("Luban", "free : " + (free >> 20) + "MB, need : " + (allocation >> 20) + "MB");
+        return allocation < free;
+    }
+
+    private Bitmap rotatingImage(Bitmap bitmap) {
+        if (srcExif == null) return bitmap;
+
+        Matrix matrix = new Matrix();
+        int angle = 0;
+        int orientation = srcExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                angle = 90;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                angle = 180;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                angle = 270;
+                break;
+        }
+
+        matrix.postRotate(angle);
+        if (angle == 0) {
+            return bitmap;
+        } else {
+            return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        }
+
+    }
 }
